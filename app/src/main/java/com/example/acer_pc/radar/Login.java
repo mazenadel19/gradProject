2be4550d4ece82dc4a746989886513f349b0e088
mazenadel19 <mazenadel19@gmail.com>
2017-04-28 18:10:54 +0200

    RentaV11: Login with FB done working on navigationBar & Register

--- app/src/main/java/com/example/acer_pc/radar/Login.java
+++ app/src/main/java/com/example/acer_pc/radar/Login.java
@@ -1,9 +1,18 @@
 package com.example.acer_pc.radar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import static android.R.attr.id;
 
@@ -13,14 +22,77 @@ import static android.R.attr.id;
 
 public class Login extends MainActivity {
 
+    LoginButton login_button;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        intializeControl();
+        loginWithFB();
 
         //toastCat.cancel();
     }
+
+
+    private void intializeControl(){
+        callbackManager = CallbackManager.Factory.create();
+
+        login_button = (LoginButton) findViewById(R.id.login_button);
+
+
+    }
+
+
+
+    private void loginWithFB(){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(),"Successfully Logged In",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),"Log In Cancelled",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),"Log In Error",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+
+
+        if(id==R.id.signUpId){
+            Intent intent= new Intent(this,Register.class); //intent(from here,to.....)
+            startActivity(intent);
+
+            Toast.makeText(getApplicationContext(),"Register",Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
 
 
 }
--- app/src/main/java/com/example/acer_pc/radar/Register.java
+++ app/src/main/java/com/example/acer_pc/radar/Register.java
@@ -1,8 +1,17 @@
 package com.example.acer_pc.radar;
 
+import android.os.Bundle;
+
 /**
  * Created by Acer-PC on 19-Apr-17.
  */
 
-public class Register {
+public class Register extends Login {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
 }
